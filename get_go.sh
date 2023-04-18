c7a0a365d9d0866a9995bd242ede4e8d7c41937e
Rohitrajak1807 <rrajak@platform9.com>
2023-04-18 18:07:47 +0530

    remove and remake

--- get_go.sh
+++ get_go.sh
@@ -12,7 +12,9 @@ ARCH=amd64
 pushd $HOME || exit
 curl -LO "https://golang.org/dl/go$GO_VERSION.$OS-$ARCH.tar.gz"
 # Extract the Go binary to $HOME/go
-sudo tar -C /usr/loca/ -xzf go$GO_VERSION.$OS-$ARCH.tar.gz
+rm -rf go
+mkdir go
+sudo tar -C ./go -xzf go$GO_VERSION.$OS-$ARCH.tar.gz
 popd || exit
 # Add Go to the PATH environment variable
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
