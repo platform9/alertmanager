99b66caa9fe132414e0918571ffa4372d0c83680
Rohitrajak1807 <rrajak@platform9.com>
2023-04-18 20:38:22 +0530

    gimme again?

--- get_go.sh
+++ get_go.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -x
-
-# Set the Go version you want to install
-GO_VERSION=1.17.13
-
-# Set the OS and architecture
-OS=linux
-ARCH=amd64
-
 # Download the Go binary
 pushd $HOME || exit
 curl -sL -o ~/.local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
